b7c3bd806ffef60f858b24ea292a8094880a784d
Steven Lee <admin@alitrack.com>
2019-04-30 08:51:41 +0800

    Create setupdb.bat

---
+++ setupdb.bat
@@ -0,0 +1,4 @@
+osql -Usa -Pdotcom -S(local)\alitrack -Q"create database alitrack"
+osql -Usa -Pdotcom -S(local)\alitrack -Q"select name from sysdatabases"
+%SystemRoot%\SysWOW64\odbcad32.exe
+pause
